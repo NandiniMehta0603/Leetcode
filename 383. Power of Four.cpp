bdad605d4f18484e0e6ebfa87a574545c2f793df
Nandini Mehta <84312981+NandiniMehta0603@users.noreply.github.com>
2023-10-24 00:01:35 +0530

    Easy

---
+++ 383. Power of Four.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        for(int i=0;i<=15;i++){
+            int temp=pow(4,i);
+            if(temp==n){
+                return true;
+            }
+            if(temp>n){
+                return false;
+            }
+        }
+        return false;
+    }
+};
